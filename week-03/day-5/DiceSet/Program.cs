d820993d4403ad766555af4ffac8faa163ee8e13
lukasz1mroz <lukasz1mroz@gmail.com>
2019-09-29 19:54:55 +0200

    updated DiceSet

--- week-03/day-5/DiceSet/Program.cs
+++ week-03/day-5/DiceSet/Program.cs
@@ -7,7 +7,7 @@ namespace Dice
     {
         public static void Main(string[] argv)
         {
-            // You have a `DiceSet` class which has a list for 6 dice
+            // You have a `DiceSet` class which has a list for 6 dice 
             // You can roll all of them with roll()
             // Check the current rolled numbers with getCurrent()
             // You can reroll with reroll()
