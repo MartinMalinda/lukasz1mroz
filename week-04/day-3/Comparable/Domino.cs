fd75a23ffd106e4f9c60490ae3edd245ae9997f8
lukasz1mroz <lukasz1mroz@gmail.com>
2019-10-02 20:24:57 +0200

    comparable exercise domino part

---
+++ week-04/day-3/Comparable/Domino.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Comparable
+{
+    public class Domino : IComparable<Domino>
+    {
+
+        // List for storing domino values
+        private readonly int[] values;
+
+        // Domino constructor
+        public Domino(int valueA, int valueB)
+        {
+            this.values = new int[] { valueA, valueB };
+        }
+
+        // Getting dominovalues
+        public int[] GetValues()
+        {
+            return values;
+        }
+
+        // Comparing dominos
+        public int CompareTo(Domino other)
+        {
+            return this.values[0].CompareTo(other.values[0]);
+        }
+    }
+}
