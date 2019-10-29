236060250a7c20eb0576fbeebc506aca9727924b
lukasz1mroz <lukasz1mroz@gmail.com>
2019-10-29 18:29:47 +0100

    Added BankOfSimba exercise

---
+++ week-07/day-2/BankOfSimba/BankOfSimba/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using BankOfSimba.Models;
+
+namespace BankOfSimba.Controllers
+{
+    public class HomeController : Controller
+    {
+        static List<BankAccount> accountsList = new List<BankAccount>()
+        {
+            new BankAccount("Tim", 750, "monkey", false, false),
+            new BankAccount("Paul", 1750, "parrot", false, false),
+            new BankAccount("John", 120, "turtle", false, true),
+            new BankAccount("Jenny", 2250, "zebra", false, true),
+            new BankAccount("Clark", 230, "bug", false, false),
+            new BankAccount("Simba", 230, "lion", true, false)
+        };
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("/show")]
+        public IActionResult Show()
+        {
+            BankAccount bankAccount = new BankAccount("Simba", 2000, "lion", false, false);
+            return View(bankAccount);
+        }
+
+        [Route("/string")]
+        public IActionResult String()
+        { 
+            ViewBag.Text = "This is an <em>HTML</em> text. <b>Enjoy yourself!</b>";
+            return View();  
+        }
+
+        [Route("/accounts")]
+        public IActionResult Accounts()
+        {
+
+            ViewBag.Items = accountsList;
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ week-07/day-2/BankOfSimba/BankOfSimba/Models/BankAccount.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BankOfSimba.Models
+{
+    public class BankAccount
+    {
+        private static int counter = 1;
+        public int Count { get; set; }
+        public string Name { get; set; }
+        public int Balance { get; set; }
+        public string AnimalType { get; set; }
+        public bool IsKing { get; set; }
+        public bool IsGood { get; set; }
+
+        public BankAccount(string name, int balance, string animalType, bool isKing, bool isGood)
+        {
+            this.Count = counter;
+            counter++;
+            this.Name = name;
+            this.Balance = balance;
+            this.AnimalType = animalType;
+            this.IsKing = isKing;
+            this.IsGood = isGood;
+        }
+    }
+}
